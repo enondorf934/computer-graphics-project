b635f68b8ab77683eee269378686e8f74be944c4
David Estes McKnight <belmont95@live.com>
2018-03-12 05:43:29 -0500

    Set up some preliminary classes for the project
    Set up some of the classes that'll contain information for drawing to the GL objects

---
+++ computer-graphics-project/src/main/java/drawables/Cloud.java
@@ -0,0 +1,37 @@
+package drawables;
+
+import javax.media.opengl.GL2;
+
+/**
+ * A class for holding information about a cloud to be drawn
+ * @author DEMcKnight
+ */
+public class Cloud implements Drawable, Shape
+{
+
+	//Class (static) variables
+	
+	//Instance variables
+		
+	//Constructors
+
+	//Class (static) methods
+	
+	//Instance methods
+	
+	//Interface methods
+	
+	@Override
+	public void draw(GL2 gl)
+	{
+		// TODO Auto-generated method stub		
+	}
+
+	@Override
+	public boolean containsPoint(double x, double y)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
