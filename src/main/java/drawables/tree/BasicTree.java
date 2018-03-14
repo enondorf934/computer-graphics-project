f0331e008079514f4a1dfe89305ca15e1fa70b24
David Estes McKnight <belmont95@live.com>
2018-03-14 07:22:28 -0500

    Finally can add the Vec2 and Vec3 classes. Pretty happy with how they turned out.

--- src/main/java/drawables/tree/BasicTree.java
+++ src/main/java/drawables/tree/BasicTree.java
@@ -2,9 +2,11 @@ package drawables.tree;
 
 import java.util.ArrayList;
 import javax.media.opengl.GL2;
+import java.awt.Color;
 
 import drawables.Drawable;
 import drawables.Shape;
+import graphicsPrimitives.Vec2;
 
 /**
  * A class for holding information about the tree to be drawn
@@ -13,19 +15,35 @@ import drawables.Shape;
 public class BasicTree implements Drawable, Shape
 {
 
+	//Class (static) variables
+
 	//Instance variables
+	Vec2 position;
+	double width;
+	double height;
+	Color trunkColor;
 	
 	/**
 	 * The tree's branches
 	 */
 	private ArrayList<BasicBranch> branches = new ArrayList<BasicBranch>();
 	
-	
-	//Class (static) variables
-	
-	//Instance variables
-	
 	//Constructors
+	
+	/**
+	 * Constructs a tree object with the given information.
+	 * @param position The bottom-left-hand corner of the tree.
+	 * @param scaleX The scale factor for the tree in the x-direction.
+	 * @param scaleY The scale factor for the tree in the y-direction.
+	 * @param trunkColor The color of the tree's trunk.
+	 */
+	public BasicTree (Vec2 position, double scaleX, double scaleY, Color trunkColor)
+	{
+		this.position=position;
+		this.width=width;
+		this.height=height;
+		this.trunkColor=trunkColor;
+	}
 	
 	//Class (static) methods
 	
