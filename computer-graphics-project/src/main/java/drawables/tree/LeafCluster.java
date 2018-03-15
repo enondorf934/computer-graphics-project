b6120b1405a5436f759775389a849112dc6de9f5
ryanabrowning <Ryan.A.Browning-1@ou.edu>
2018-03-15 14:44:01 -0500

    use upper left corner to draw leaf cluster

--- computer-graphics-project/src/main/java/drawables/tree/LeafCluster.java
+++ computer-graphics-project/src/main/java/drawables/tree/LeafCluster.java
@@ -14,8 +14,8 @@ import drawables.Drawable;
  */
 public final class LeafCluster implements Drawable
 {
-  private int x;        // x-coordinate of lower left corner of leaf cluster
-  private int y;        // y-coordinate of lower left corner of leaf cluster
+  private int x;        // x-coordinate of upper left corner of leaf cluster
+  private int y;        // y-coordinate of upper left corner of leaf cluster
   private int w;        // width of leaf cluster
   private int h;        // height of leaf cluster
 
@@ -31,6 +31,12 @@ public final class LeafCluster implements Drawable
     this.y = y;
     this.w = w;
     this.h = h;
+
+    // Default green
+    r = 0.32f;
+    g = 0.68f;
+    b = 0.40f;
+    alpha = 1.0f;
   }
 
   public LeafCluster(int x, int y, int w, int h, float r, float g, float b, float alpha)
@@ -56,27 +62,27 @@ public final class LeafCluster implements Drawable
 
   public void draw(GL2 gl)
   {
-    // Outline leaf cluster
-    gl.glBegin(GL.GL_LINE_LOOP);
-    gl.glColor4f(0.0f, 0.0f, 0.0f, 1.0f); // Black
-
-    // Draw vertices clockwise starting from lower left corner
-    gl.glVertex2i(x, y);  // lower left corner
-    gl.glVertex2i(x, y + h);
-    gl.glVertex2i(x + w, y + h);
-    gl.glVertex2i(x + w, y);
-
-    gl.glEnd();
-
     // Fill leaf cluster
     gl.glBegin(GL2.GL_QUADS);
     gl.glColor4f(r, g, b, alpha);
 
-    // Draw vertices clockwise starting from lower left corner
-    gl.glVertex2i(x, y);  // lower left corner
-    gl.glVertex2i(x, y + h);
-    gl.glVertex2i(x + w, y + h);
+    // Draw vertices clockwise starting from upper left corner
+    gl.glVertex2i(x, y);  // upper left corner
     gl.glVertex2i(x + w, y);
+    gl.glVertex2i(x + w, y + h);
+    gl.glVertex2i(x, y + h);
+
+    gl.glEnd();
+
+    // Outline leaf cluster
+    gl.glBegin(GL.GL_LINE_LOOP);
+    gl.glColor4f(0.0f, 0.0f, 0.0f, 1.0f); // Black
+
+    // Draw vertices clockwise starting from upper left corner
+    gl.glVertex2i(x, y);  // upper left corner
+    gl.glVertex2i(x + w, y);
+    gl.glVertex2i(x + w, y + h);
+    gl.glVertex2i(x, y + h);
 
     gl.glEnd();
   }
