9a74194b35d73e526d5878a551ef502c0e6b16b6
ryanabrowning <Ryan.A.Browning-1@ou.edu>
2018-04-26 20:07:07 -0500

    draw trunk

--- src/main/java/drawables/tree/UnitTree.java
+++ src/main/java/drawables/tree/UnitTree.java
@@ -1,11 +1,13 @@
 package drawables.tree;
 
 import java.awt.*;
+import java.awt.geom.*;
 import java.util.*;
 import javax.media.opengl.*;
 
 import drawables.Drawable;
 import drawables.Shape;
+import reusable.Helpers;
 
 //******************************************************************************
 
@@ -33,7 +35,7 @@ public final class UnitTree implements Drawable, Shape
   private static final double LEAF_CLUSTER_HEIGHT_RATIO = 0.75;
 
   private static final Color TRUNK_COLOR = new Color(166, 129, 62);
-  private static final double TRUNK_WIDTH_RATIO = 0.1;
+  private static final double TRUNK_WIDTH_RATIO = 0.075;
   private static final double TRUNK_HEIGHT_RATIO = 0.4;
 
   private double x;   // x-coordinate of lower left corner of tree bounding box
@@ -41,7 +43,10 @@ public final class UnitTree implements Drawable, Shape
   private double w;   // maximum width of tree
   private double h;   // maximum height of tree
 
-  private ArrayList<LeafCluster> leafClusters;  // geometric leaf clusters
+  private ArrayList<Point2D.Double> trunkFillVertices;    // vertices to fill tree trunk
+  private ArrayList<Point2D.Double> trunkOutlineVertices; // vertices to outline tree trunk
+
+  private ArrayList<LeafCluster> leafClusters;            // geometric leaf clusters
 
   //****************************************************************************
 
@@ -55,6 +60,8 @@ public final class UnitTree implements Drawable, Shape
     // Initialize width and height
     w = WIDTH;
     h = HEIGHT;
+
+    calculateTrunkVertices();
 
     // Generate random leaf clusters
     generateLeafClusters();
@@ -96,15 +103,76 @@ public final class UnitTree implements Drawable, Shape
     }
   }
 
-  // FIXME
+  private void calculateTrunkVertices()
+  {
+    trunkFillVertices = new ArrayList<Point2D.Double>();
+    trunkOutlineVertices = new ArrayList<Point2D.Double>();
+
+    double center_x = w / 2;
+    double trunk_width = TRUNK_WIDTH_RATIO * w;
+    double trunk_height = TRUNK_HEIGHT_RATIO * h;
+
+    // Calculate vertices to fill tree trunk
+    trunkFillVertices.add(new Point2D.Double(center_x - (1.5 * trunk_width), y));
+    trunkFillVertices.add(new Point2D.Double(center_x - (0.5 * trunk_width), y + trunk_width));
+    trunkFillVertices.add(new Point2D.Double(center_x - (0.5 * trunk_width), y)); // remove for outline
+
+    trunkFillVertices.add(new Point2D.Double(center_x - (0.5 * trunk_width), y + trunk_height));
+    trunkFillVertices.add(new Point2D.Double(center_x + (0.5 * trunk_width), y + trunk_height));
+
+    trunkFillVertices.add(new Point2D.Double(center_x + (0.5 * trunk_width), y)); // remove for outline
+    trunkFillVertices.add(new Point2D.Double(center_x + (0.5 * trunk_width), y + trunk_width));
+    trunkFillVertices.add(new Point2D.Double(center_x + (1.5 * trunk_width), y));
+
+    // Calculate vertices to outline tree trunk (copy fill vertices)
+    for (Point2D.Double vertex : trunkFillVertices)
+    {
+      trunkOutlineVertices.add(new Point2D.Double(vertex.x, vertex.y));
+    }
+
+    // Remove fill vertices unnecessary for outline
+    trunkOutlineVertices.remove(5);
+    trunkOutlineVertices.remove(2);
+  }
+
   private void drawTrunk(GL2 gl)
   {
+    // Set trunk color
+    Helpers.setColor(gl, TRUNK_COLOR);
+
+    // Fill left triangle
+    gl.glBegin(GL2.GL_POLYGON);
+    for (int i = 0; i < 3; i++)
+    {
+      gl.glVertex2d(trunkFillVertices.get(i).x, trunkFillVertices.get(i).y);
+    }
+    gl.glEnd();
+
+    // Fill trunk
+    gl.glBegin(GL2.GL_POLYGON);
+    for (int i = 2; i < 6; i++)
+    {
+      gl.glVertex2d(trunkFillVertices.get(i).x, trunkFillVertices.get(i).y);
+    }
+    gl.glEnd();
+
+    // Fill right triangle
+    gl.glBegin(GL2.GL_POLYGON);
+    for (int i = 5; i < 8; i++)
+    {
+      gl.glVertex2d(trunkFillVertices.get(i).x, trunkFillVertices.get(i).y);
+    }
+    gl.glEnd();
+
+    // Outline trunk
     gl.glBegin(GL.GL_LINE_LOOP);
     gl.glColor4f(0.0f, 0.0f, 0.0f, 1.0f); // Black
-    gl.glVertex2d(x, y);
-    gl.glVertex2d(x, y + h);
-    gl.glVertex2d(x + w, y + h);
-    gl.glVertex2d(x + w, y);
+
+    for (Point2D.Double vertex : trunkOutlineVertices)
+    {
+      gl.glVertex2d(vertex.x, vertex.y);
+    }
+
     gl.glEnd();
   }
 
