7012944df9c6a15bee90150addb0db1a7f240b4f
ryanabrowning <Ryan.A.Browning-1@ou.edu>
2018-04-26 20:26:01 -0500

    made minor formatting changes

--- src/main/java/drawables/tree/UnitTree.java
+++ src/main/java/drawables/tree/UnitTree.java
@@ -23,6 +23,10 @@ public final class UnitTree implements Drawable, Shape
   private static final double WIDTH = 300.0;
   private static final double HEIGHT = 500.0;
 
+  private static final Color TRUNK_COLOR = new Color(166, 129, 62);
+  private static final double TRUNK_WIDTH_RATIO = 0.075;
+  private static final double TRUNK_HEIGHT_RATIO = 0.4;
+
   private static final int MIN_NUM_LEAF_CLUSTERS = 12;
   private static final int MAX_NUM_LEAF_CLUSTERS = 20;
 
@@ -34,14 +38,20 @@ public final class UnitTree implements Drawable, Shape
 
   private static final double LEAF_CLUSTER_HEIGHT_RATIO = 0.75;
 
-  private static final Color TRUNK_COLOR = new Color(166, 129, 62);
-  private static final double TRUNK_WIDTH_RATIO = 0.075;
-  private static final double TRUNK_HEIGHT_RATIO = 0.4;
+  //****************************************************************************
 
   private double x;   // x-coordinate of lower left corner of tree bounding box
   private double y;   // y-coordinate of lower left corner of tree bounding box
   private double w;   // maximum width of tree
   private double h;   // maximum height of tree
+
+  private double trunkWidth;
+  private double trunkHeight;
+
+  private double minLeafWidth;
+  private double maxLeafWidth;
+  private double minLeafHeight;
+  private double maxLeafHeight;
 
   private ArrayList<Point2D.Double> trunkFillVertices;    // vertices to fill tree trunk
   private ArrayList<Point2D.Double> trunkOutlineVertices; // vertices to outline tree trunk
@@ -61,9 +71,18 @@ public final class UnitTree implements Drawable, Shape
     w = WIDTH;
     h = HEIGHT;
 
+    // Initialize trunk
+    trunkWidth = TRUNK_WIDTH_RATIO * w;
+    trunkHeight = TRUNK_HEIGHT_RATIO * h;
+
     calculateTrunkVertices();
 
-    // Generate random leaf clusters
+    // Initialize minimum and maximum leaf cluster values
+    minLeafWidth = MIN_LEAF_WIDTH_RATI0 * w;
+    maxLeafWidth = MAX_LEAF_WIDTH_RATIO * w;
+    minLeafHeight = MIN_LEAF_HEIGHT_RATI0 * h;
+    maxLeafHeight = MAX_LEAF_HEIGHT_RATIO * h;
+
     generateLeafClusters();
   }
 
@@ -75,6 +94,37 @@ public final class UnitTree implements Drawable, Shape
   }
 
   //****************************************************************************
+
+  private void calculateTrunkVertices()
+  {
+    trunkFillVertices = new ArrayList<Point2D.Double>();
+    trunkOutlineVertices = new ArrayList<Point2D.Double>();
+
+    double center_x = w / 2;
+
+
+    // Calculate vertices to fill tree trunk
+    trunkFillVertices.add(new Point2D.Double(center_x - (1.5 * trunkWidth), y));
+    trunkFillVertices.add(new Point2D.Double(center_x - (0.5 * trunkWidth), y + trunkWidth));
+    trunkFillVertices.add(new Point2D.Double(center_x - (0.5 * trunkWidth), y)); // remove for outline
+
+    trunkFillVertices.add(new Point2D.Double(center_x - (0.5 * trunkWidth), y + trunkHeight));
+    trunkFillVertices.add(new Point2D.Double(center_x + (0.5 * trunkWidth), y + trunkHeight));
+
+    trunkFillVertices.add(new Point2D.Double(center_x + (0.5 * trunkWidth), y)); // remove for outline
+    trunkFillVertices.add(new Point2D.Double(center_x + (0.5 * trunkWidth), y + trunkWidth));
+    trunkFillVertices.add(new Point2D.Double(center_x + (1.5 * trunkWidth), y));
+
+    // Calculate vertices to outline tree trunk (copy fill vertices)
+    for (Point2D.Double vertex : trunkFillVertices)
+    {
+      trunkOutlineVertices.add(new Point2D.Double(vertex.x, vertex.y));
+    }
+
+    // Remove fill vertices unnecessary for outline
+    trunkOutlineVertices.remove(5);
+    trunkOutlineVertices.remove(2);
+  }
 
   private void generateLeafClusters()
   {
@@ -88,51 +138,19 @@ public final class UnitTree implements Drawable, Shape
     {
       // Generate random leaf cluster width (height) relative to tree width (height)
       // and within minimum and maximum ratios of tree width (height)
-      double leaf_w = ((MAX_LEAF_WIDTH_RATIO - MIN_LEAF_WIDTH_RATI0) * r.nextDouble() + MIN_LEAF_WIDTH_RATI0) * w;
-      double leaf_h = ((MAX_LEAF_HEIGHT_RATIO - MIN_LEAF_HEIGHT_RATI0) * r.nextDouble() + MIN_LEAF_HEIGHT_RATI0) * h;
+      double leafWidth = (maxLeafWidth - minLeafWidth) * r.nextDouble() + minLeafWidth;
+      double leafHeight = (maxLeafHeight - minLeafHeight) * r.nextDouble() + minLeafHeight;
 
       // Generate random leaf cluster position relative to tree width and hight
       // and given leaf cluster width and height
-      double max_x = x + w - leaf_w;
-      double max_y = y + h - leaf_h;
+      double max_x = x + w - leafWidth;
+      double max_y = y + h - leafHeight;
       double min_y = (1 - LEAF_CLUSTER_HEIGHT_RATIO) * h + y;
       double leaf_x = (max_x - x) * r.nextDouble() + x;
       double leaf_y = (max_y - min_y) * r.nextDouble() + min_y;
 
       leafClusters.add(new LeafCluster(leaf_x, leaf_y, leaf_w, leaf_h));
     }
-  }
-
-  private void calculateTrunkVertices()
-  {
-    trunkFillVertices = new ArrayList<Point2D.Double>();
-    trunkOutlineVertices = new ArrayList<Point2D.Double>();
-
-    double center_x = w / 2;
-    double trunk_width = TRUNK_WIDTH_RATIO * w;
-    double trunk_height = TRUNK_HEIGHT_RATIO * h;
-
-    // Calculate vertices to fill tree trunk
-    trunkFillVertices.add(new Point2D.Double(center_x - (1.5 * trunk_width), y));
-    trunkFillVertices.add(new Point2D.Double(center_x - (0.5 * trunk_width), y + trunk_width));
-    trunkFillVertices.add(new Point2D.Double(center_x - (0.5 * trunk_width), y)); // remove for outline
-
-    trunkFillVertices.add(new Point2D.Double(center_x - (0.5 * trunk_width), y + trunk_height));
-    trunkFillVertices.add(new Point2D.Double(center_x + (0.5 * trunk_width), y + trunk_height));
-
-    trunkFillVertices.add(new Point2D.Double(center_x + (0.5 * trunk_width), y)); // remove for outline
-    trunkFillVertices.add(new Point2D.Double(center_x + (0.5 * trunk_width), y + trunk_width));
-    trunkFillVertices.add(new Point2D.Double(center_x + (1.5 * trunk_width), y));
-
-    // Calculate vertices to outline tree trunk (copy fill vertices)
-    for (Point2D.Double vertex : trunkFillVertices)
-    {
-      trunkOutlineVertices.add(new Point2D.Double(vertex.x, vertex.y));
-    }
-
-    // Remove fill vertices unnecessary for outline
-    trunkOutlineVertices.remove(5);
-    trunkOutlineVertices.remove(2);
   }
 
   private void drawTrunk(GL2 gl)
